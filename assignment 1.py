edde29123f27e36bcec1dd45bb6d8b2f561eb56c
mitalikumari <89791838+mitalikumari@users.noreply.github.com>
2021-08-31 16:45:49 +0530

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,7 @@
+a = int(input("Enter number a: "))
+b = int(input("Enter number b: "))
+print("Before swapping the number a: ",a,"b: ",b)
+temp = a
+a = b
+b = temp
+print("After swapping the number a: ",a,"b: ",b)
