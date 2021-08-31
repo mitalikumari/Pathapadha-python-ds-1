110f747e7bf494feb4d604d726d3a9bedb118197
mitalikumari <89791838+mitalikumari@users.noreply.github.com>
2021-08-31 16:52:21 +0530

    Add files via upload

--- assignment 1.py
+++ assignment 1.py
@@ -5,3 +5,11 @@ temp = a
 a = b
 b = temp
 print("After swapping the number a: ",a,"b: ",b)
+
+
+swapping without using third variable
+a = int(input("Enter number a: "))
+b = int(input("Enter number b: "))
+print("Before swapping the number a: ",a,"b: ",b)
+a,b = b,a
+print("After swapping the number a: ",a,"b: ",b)
